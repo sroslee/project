4471b97486ec52a989a5d46d80d3e50f7af3d781
Aurora Toutant <atoutant@wisc.edu>
2024-04-18 11:58:20 -0500

    added comments to preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -5,7 +5,8 @@
 wget https://dax-cdn.cdn.appdomain.cloud/dax-airline/1.0.1/airline.tar.gz
 tar -xzf airlines.tar.gz
 
-
+# split by column 7 AA DL F9 WN UA
+# keep 3 (month), 5 (dayofweek), 17 (state of origin), 30 (crsdeptime), 32 (departure delay), 48 (cancelled), 55 (distance), 62 (1st departure time),  
 #split data by desired areas into many smaller files
 
 
