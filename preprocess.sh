5d5fe0cf19dc3cb438b10eb92421dacc7165c5d9
Simran Desai <sadesai3@wisc.edu>
2024-04-19 12:15:04 -0500

    adding

--- preprocess.sh
+++ preprocess.sh
@@ -1,20 +1,25 @@
-	#!/bin/bash
+#!/bin/bash
 
 #download data and unpack --> becomes airlines.csv
 
-wget https://dax-cdn.cdn.appdomain.cloud/dax-airline/1.0.1/airline.tar.gz
-tar -xzf airlines.tar.gz
+#wget https://dax-cdn.cdn.appdomain.cloud/dax-airline/1.0.1/airline.tar.gz
+tar -xzf airline.tar.gz
 
 
 #split data by desired areas into many smaller files
 
+cat airline.csv | cut -d, -f3,5,7,17,30,32,48,55,62 | awk -F, '{ if ($3 == "AA") {print $0} }' > American.csv
+cat airline.csv | cut -d, -f3,5,7,17,30,32,48,55,62 | awk -F, '{ if ($3 == "DL") {print $0} }' > Delta.csv
+cat airline.csv | cut -d, -f3,5,7,17,30,32,48,55,62 | awk -F, '{ if ($3 == "F9") {print $0} }' > Frontier.csv
+cat airline.csv | cut -d, -f3,5,7,17,30,32,48,55,62 | awk -F, '{ if ($3 == "WN") {print $0} }' > Southwest.csv
+cat airline.csv | cut -d, -f3,5,7,17,30,32,48,55,62 | awk -F, '{ if ($3 == "UA") {print $0} }' > United.csv
 
 
 
 #remove big data file but keep split up files
 
-rm airlines.tar.gz
-rm airlines.csv
+rm airline.tar.gz
+rm airline.csv
 
 #ls the smaller file names into input_file_list
-#ls <file names> > input_file_list
+ls *.csv > input_file_list
