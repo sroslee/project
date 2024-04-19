f8517da5860e5beb7b2413da84ffbe9f4d9fefe9
Nuha Roslee <sroslee@wisc.edu>
2024-04-19 12:19:18 -0500

    Adding new R lines

--- Rscript.R
+++ Rscript.R
@@ -14,3 +14,15 @@ if(length(args) == 1){
 data = read.csv(dataframe)
 
 
+response = data$arr_delay
+predictor1 = data$distance
+#predictor2 = data$flights
+predictor3 = data$actual_elapsed_time
+predictor4 = data$dep_delay
+predictor5 = data$month
+
+model = glm(response ~ predictor1 + predictor2 + predictor3 + predictor4 + as.factor(predictor5) + predictor1:predictor4 + predictor4:as.factor(predictor5))
+print(summary(model))
+
+predictions <- predict(model, newdata = new_data, type = "response")
+write.csv(predictions, file = "predicted.csv")
